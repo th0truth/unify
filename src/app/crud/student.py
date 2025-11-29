765e9fe37a11214850707e914ec556802f34bece
th0truth <shiftrams@gmail.com>
2025-11-30 00:31:13 +0200

    feat(crud): fetch student

--- src/app/crud/student.py
+++ src/app/crud/student.py
@@ -1,8 +1,19 @@
+from fastapi import status, HTTPException
+from core.schemas.student import StudentBase
+
 from .base import BaseCRUD
 
 class StudentCRUD(BaseCRUD):
   def __init__(self, db):
     super().__init__(db)
+
+  async def _fetch(self, edbo_id: int):
+    if not (user := await self.db["students"].find_one({"edbo_id": edbo_id})):
+      raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Student not found."
+      )
+    return StudentBase.model_validate(user)
 
   async def get_grades(self, *, edbo_id: int, group: str, **kwargs) -> dict:
     """Get student subject grades."""
