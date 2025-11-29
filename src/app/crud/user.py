ec26ef5fcd148d528cbe5cab4fabb40b15f56c98
th0truth <shiftrams@gmail.com>
2025-11-29 14:58:34 +0200

    feat(crud): add/update new methods to UserCRUD such as find, delete, authenticate

--- src/app/crud/user.py
+++ src/app/crud/user.py
@@ -1,25 +1,32 @@
 from typing import Optional, List, Union
 
 from core.config import ModelType
+from core.logger import logger
 from core.security.utils import Hash
-
 from .base import BaseCRUD
 
 class UserCRUD(BaseCRUD):
   def __init__(self, db):
     super().__init__(db)
 
-  async def find(self, *, username: Union[str, int], exclude: Optional[List] = None) -> dict:
-    """Fetch user profile from MongoDB database."""
-    for collection in await self.db.list_collection_names():
-      if (user := await self.db[collection].find_one({"edbo_id": int(username)} if isinstance(username, int) or username.isdigit() else {"email.address": username})):
-        break
-    if exclude:
-      for key in exclude:
-        user.pop(key)
-    return user
+  async def find(self, *, username: str, exclude: Optional[List] = None) -> Union[dict, None]:
+    """Fetches user profile using username."""
+    try:
+      for collection in await self.db.list_collection_names():
+        if (user := await self.db[collection].find_one(
+          {"edbo_id": int(username)}if isinstance(username, int) or username.isdigit() else {"email.address": username})
+        ):
+          break
+      if exclude:
+        for key in exclude:
+          user.pop(key, None)
+      return user
+    except Exception as e:
+      logger.error({"message": "[x] An error occured while searching user profile in MongoDB.", "detail": str(e)}, exc_info=True)
+      return
 
   async def create(self, user: ModelType):
+    """Creates a user profile."""
     user.password = Hash.hash(plain=user.password)
     await self.db[user.role].insert_one(user.model_dump())
     return user
@@ -33,19 +40,19 @@ class UserCRUD(BaseCRUD):
       update={"$set": update}
     )
 
-  async def delete(self, username: Union[str, int]):
-    """Deletes a user."""
+  async def delete(self, username: Union[str, int]) -> Union[int, None]:
+    """Deletes a user profile."""
     if not (user := await self.find(username=username)):
       return
     result = await self.db[user.get("role")].delete_one(user)
     return result.deleted_count
 
   async def authenticate(self, *, username: Union[str, int], plain_pwd: str, exclude: Optional[List] = None) -> Union[dict, None]:
-    """Authenticate user using credentials."""
+    """Authenticates a user using credentials."""
     user = await self.find(username=username)
     if not user or not Hash.verify(plain_pwd, user.get("password")):
       return
     if exclude:
       for key in exclude:
-        user.pop(key)
+        user.pop(key, None)
     return user
