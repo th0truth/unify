a670d3b1830190cf2a492704f8249fd771ffe36e
th0truth <shiftrams@gmail.com>
2025-11-29 14:59:06 +0200

    feat(crud): add/update new methods to BaseCRUD such as read_all, update, update_all

--- src/app/crud/base.py
+++ src/app/crud/base.py
@@ -8,19 +8,24 @@ class BaseCRUD:
     self.db = db
 
   async def create(self, collection: str, model: ModelType):
+    """Creates an object."""
     await self.db[collection].insert_one(model.model_dump())
 
   async def read(self, collection: str, filter: Any):
+    """Reads specific object."""
     return await self.db[collection].find_one(filter)
 
+  async def read_all(self, collection: str, *, filter: Any = {}, offset: int = 0, length: Optional[int] = None):    
+    """Reads all objects."""
+    objects = await self.db[collection].find(filter).to_list(length)
+    return objects[offset:]
+
   async def update(self, collection: str, *, update: dict, filter: Any = {}):
+    """Updates an object."""
     result = await self.db[collection].update_one(filter, update={"$set": update})
     return result.modified_count
 
   async def update_all(self, collection: str, *, update: dict, filter: Any = {}) -> int:
+    """Updates all objects."""
     result = await self.db[collection].update_many(filter, update={"$set": update})
     return result.modified_count
-  
-  async def read_all(self, collection: str, *, filter: Any = {}, projection: Optional[dict] = None, offset: int = 0, length: Optional[int] = None):
-    objects = await self.db[collection].find(filter, projection=projection).to_list(length)
-    return objects[offset:]
