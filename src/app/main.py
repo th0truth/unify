39769c7fc450b0b3881d8ee256c3191949ac92da
th0truth <shiftrams@gmail.com>
2025-11-29 12:24:07 +0200

    feat(main): add CORS as middleware

--- src/app/main.py
+++ src/app/main.py
@@ -2,38 +2,46 @@ from starlette.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+# Local Dependencies
 from core.config import settings
 from core.db import (
   MongoClient,
   RedisClient
 )
-from api.api import api_router
+from api.api import api_main_router
 
+# Initilize lifespan events
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-  await RedisClient.connect()
-  await MongoClient.connect()
   try:
+    await RedisClient.connect()
+    await MongoClient.connect()
     yield
   finally:
     await MongoClient.close()
     await RedisClient.close()
 
+
+# Initialize an app
 app = FastAPI(
   title=settings.NAME,
   description=settings.DESCRIPTION,
   summary=settings.SUMMARY,
   version=settings.VERSION,
-  openapi_url=f"{settings.API_V1_STR}/openapi.json",
+  openapi_url="/api/openapi.json",
   lifespan=lifespan
 )
 
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"]
-)
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+  # Add middlewares
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.all_cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+  )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
+# Include main router to the app
+app.include_router(api_main_router)
