f752087b9ad7d603850bfabe3df9f0f292beb9c7
th0truth <shiftrams@gmail.com>
2026-01-24 14:15:53 +0200

    feat: implement RateLimitMiddleware based on user role

---
+++ src/app/core/middleware/limiter.py
@@ -0,0 +1,21 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from slowapi.util import get_remote_address
+
+from core.config import settings, RATE_LIMITS
+from api.dependencies import get_jwt_payload
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+  def __init__(self, app, dispatch = None):
+    super().__init__(app, dispatch)
+
+  async def dispatch(self, request, call_next):
+    if (payload := get_jwt_payload(request)):
+      role, jti = payload.get("role"), payload.get("jti")  
+      request.state.limit_value = RATE_LIMITS.get(role, settings.RATE_LIMIT_ANONYMOUS)
+      request.state.identifier = f"{role}:{jti}"
+    else:
+      request.state.limit_value = settings.RATE_LIMIT_ANONYMOUS
+      request.state.identifier = f"anonymous:{get_remote_address(request)}"
+
+    response = await call_next(request)
+    return response
