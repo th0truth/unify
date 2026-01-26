eda0a3901fec30db2e18447a47ae23dd581224ba
th0truth <shiftrams@gmail.com>
2026-01-26 21:21:33 +0200

    feat: add health

---
+++ src/app/api/v1/routers/health.py
@@ -0,0 +1,18 @@
+from fastapi import (
+  APIRouter,
+  status
+)
+from core.schemas.etc import HealthCheck
+from api.dependencies import limiter
+
+router = APIRouter(tags=["Health"])
+
+
+@router.get("",
+  summary="Perform a Health Check",
+  response_description="Return HTTP Status Code 200 (OK)",
+  status_code=status.HTTP_200_OK,
+  response_model=HealthCheck)
+@limiter.exempt
+async def healt_check():
+  return HealthCheck(status="ok")
