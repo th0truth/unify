78c236866d979fcfe5a4a506246d11a0c05183c0
th0truth <shiftrams@gmail.com>
2026-01-26 21:20:32 +0200

    refactor: remove health from main file

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,10 @@
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, status, Request
 from contextlib import asynccontextmanager
+from fastapi import FastAPI
 import itsdangerous
 
 # Rate Limiting Dependencies
-from slowapi import Limiter
 from slowapi.errors import RateLimitExceeded
 
 # Local Dependencies
@@ -13,13 +12,12 @@ from core.middleware.limiter import RateLimitMiddleware
 from core.errors import rate_limit_exceeded_handler
 
 from core.logger import logger
-from core.config import settings, REDIS_URI
-from core.schemas.etc import HealthCheck
+from core.config import settings
 from core.database import (
   MongoClient,
   RedisClient
 )
-from api.dependencies import get_identifier
+from api.dependencies import limiter
 from api.api import api_main_router
 
 # Initilize lifespan events
@@ -46,17 +44,6 @@ app = FastAPI(
 )
 
 
-# Initialize rate limiter
-limiter = Limiter(
-  key_func=get_identifier,
-  default_limits=["20/minute"],
-  strategy="moving-window",
-  storage_uri=REDIS_URI,
-  headers_enabled=False,
-  swallow_errors=False
-)
-
-
 # Add middleware RateLimitMiddleware
 app.add_middleware(RateLimitMiddleware)
 app.add_middleware(
@@ -71,18 +58,6 @@ app.add_middleware(
 # Attach limiter to the app
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
-
-@app.get("/health",
-  tags=["Health Check"],
-  summary="Perform a Health Check",
-  response_description="Return HTTP Status Code 200 (OK)",
-  status_code=status.HTTP_200_OK,
-  response_model=HealthCheck)
-@limiter.exempt
-async def healt_check(
-  request: Request
-):
-  return HealthCheck(status="ok")
 
 # Set all CORS enabled origins
 if settings.all_cors_origins:
