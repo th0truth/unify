0f84e305cd23b3a4cdaff27049106172168b2f8e
th0truth <shiftrams@gmail.com>
2026-02-11 10:52:11 +0200

    feat: add CRUD classes

--- src/app/crud/__init__.py
+++ src/app/crud/__init__.py
@@ -2,10 +2,14 @@ __all__ = [
   "BaseCRUD",
   "UserCRUD",
   "StudentCRUD",
-  "GradeCRUD"
+  "GradeCRUD",
+  "ScheduleCRUD",
+  "GroupCRUD",
 ]
 
 from .base import BaseCRUD
 from .user import UserCRUD
 from .student import StudentCRUD
 from .grade import GradeCRUD
+from .schedule import ScheduleCRUD
+from .group import GroupCRUD
